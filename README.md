65af06b108409221f19ef1760e4ed7b6f186dcdf
unknown <v.arc1057@gmail.com>
2025-02-26 07:41:58 -0700

    [INICIA] el proyecto

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # calculadora_android
+Esta es una aplicación de calculadora sencilla
+y debería cumplir con lo siguiente.
+[ ] interfaz de máquina de estados por controlar el comportamiento
+[ ] calcular al menos 4 operaciones básicas de una calculadora
+    [ ] suma
+    [ ] resta
+    [ ] multiplicación
+    [ ] división
---
+++ app/src/main/java/mx/alumnos/uacj/dora_la_calculadora/MainActivity.kt
@@ -0,0 +1,47 @@
+package mx.alumnos.uacj.dora_la_calculadora
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import mx.alumnos.uacj.dora_la_calculadora.ui.theme.Dora_la_calculadoraTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Dora_la_calculadoraTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Dora_la_calculadoraTheme {
+        Greeting("Android")
+    }
+}
